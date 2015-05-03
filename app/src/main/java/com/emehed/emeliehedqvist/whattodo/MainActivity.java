436a0993f80869c7bb2f4d6e948ba37da8d623c8
micfri <micfritiofsson@gmail.com>
2015-05-03 15:18:45 +0200

    oklara förändringar

--- app/src/main/java/com/emehed/emeliehedqvist/whattodo/MainActivity.java
+++ app/src/main/java/com/emehed/emeliehedqvist/whattodo/MainActivity.java
@@ -1,17 +1,16 @@
 package com.emehed.emeliehedqvist.whattodo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+class MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(res.layout.activity_main);
     }
 
 
