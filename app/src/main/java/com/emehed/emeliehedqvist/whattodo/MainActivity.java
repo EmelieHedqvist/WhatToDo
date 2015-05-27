98270ea6cc72ec57c7ee580c7d3bf58fcf62cdac
taljedal <henrik@taljedal.se>
2015-05-27 14:01:24 +0200

    Code cleaning

--- app/src/main/java/com/emehed/emeliehedqvist/whattodo/MainActivity.java
+++ app/src/main/java/com/emehed/emeliehedqvist/whattodo/MainActivity.java
@@ -3,18 +3,15 @@ package com.emehed.emeliehedqvist.whattodo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
-<<<<<<< HEAD
-public class MainActivity extends ActionBarActivity{
-=======
+
 public class MainActivity extends Activity {
     public static final String EXTRA_MESSAGE = "com.emehed.emeliehedqvist.whattodo.MESSAGE";
->>>>>>> KnapparWithPlaceFinder
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
